fe5b0b66d01bd39ca871e21b44534abf55cb930e
Luciana Cedro <--global>
2022-07-24 16:38:50 -0300

    corrigindo conflitos

--- lib/app/services/projects/project_service.dart
+++ lib/app/services/projects/project_service.dart
@@ -2,6 +2,6 @@ import 'package:job_timer/app/entities/projects_status.dart';
 import 'package:job_timer/app/view_models/project_model.dart';
 
 abstract class ProjectService {
-  Future<void> register(ProjectModel projectModel);
+  Future<void> register(ProjectModel project);
   Future<List<ProjectModel>> findByStatus(ProjectStatus status);
 }

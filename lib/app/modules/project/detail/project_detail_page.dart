7a50fcc78d8305e0266499bf16d4ffdf8c08cc93
Luciana Cedro <--global>
2022-07-24 21:09:45 -0300

    implementando o modular para a tela de detalhes

--- lib/app/modules/project/detail/project_detail_page.dart
+++ lib/app/modules/project/detail/project_detail_page.dart
@@ -1,11 +1,14 @@
 import 'package:flutter/material.dart';
 import 'package:job_timer/app/core/ui/job_timer_icons.dart';
+import 'package:job_timer/app/modules/project/controller/project_detail_controller.dart';
 import 'package:job_timer/app/modules/project/detail/widgets/project_detail_appbar.dart';
 import 'package:job_timer/app/modules/project/detail/widgets/project_pie_chart.dart';
 import 'package:job_timer/app/modules/project/detail/widgets/project_task_tile.dart';
 
 class ProjectDetailPage extends StatelessWidget {
-  const ProjectDetailPage({Key? key}) : super(key: key);
+  final ProjectDetailController controller;
+  const ProjectDetailPage({Key? key, required this.controller})
+      : super(key: key);
 
   @override
   Widget build(BuildContext context) {
@@ -30,8 +33,8 @@ class ProjectDetailPage extends StatelessWidget {
                   padding: const EdgeInsets.all(15.0),
                   child: ElevatedButton.icon(
                       onPressed: () {},
-                      icon: Icon(JobTimerIcons.ok_circled2),
-                      label: Text('Finalizar projeto')),
+                      icon: const Icon(JobTimerIcons.ok_circled2),
+                      label: const Text('Finalizar projeto')),
                 ),
               )),
         ],
